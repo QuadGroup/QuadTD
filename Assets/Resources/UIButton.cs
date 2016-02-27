b3f4ae7c06d13257371ec4deeb394ea762558e38
Lego <legolax@yandex.ru>
2016-02-27 17:51:11 +0300

    Prefabs,UI,Fonts

---
+++ Assets/Resources/UIButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIButton : MonoBehaviour {
+
+    public Text ButtonText;
+    public int UpTextSize;
+    public int DownTextSize;
+
+    public void OnButtonUp()
+    {
+        ButtonText.fontSize = UpTextSize;
+    }
+
+    public void OnButtonDown()
+    {
+        ButtonText.fontSize = DownTextSize;
+    }
+}
